a5fb3680970d8b7fcb7800637994d56bd2e64cb7
hendraadi011 <hendraadisaputra011@gmail.com>
2023-10-18 14:19:11 +0700

    mengubah width dan height

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
 
-    canvas.width = 1400;
-    canvas.height = 720;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     let enemies = [];
     let score = 0;
     let gameOver = false;
